c81f3f11f3ae2512cf399a88f24a050e0676a177
Robert Smallshire <rob@sixty-north.com>
2023-08-22 12:31:41 +0200

    Actively wake from sleep on macOS

--- src/eugeroic/drivers/macos/driver.py
+++ src/eugeroic/drivers/macos/driver.py
@@ -1,13 +1,19 @@
 import contextlib
 from collections.abc import Generator
 
-from eugeroic.drivers.macos.iokit import power_management_assertion, AssertionType, Level
+from eugeroic.drivers.macos.iokit import (
+    power_management_assertion, AssertionType, Level,
+    declare_local_user_activity,
+)
+
 
 @contextlib.contextmanager
-def wakefulness(logger, reason: str) -> Generator[None, None, None]:
+def wakefulness(logger, reason: str, wake: bool=True) -> Generator[None, None, None]:
     logger.debug("Entering wakefulness state during: %r", reason)
     try:
         with power_management_assertion(AssertionType.PreventUserIdleDisplaySleep, Level.On, reason):
+            if wake:
+                declare_local_user_activity(reason)
             yield
     finally:
         logger.debug("Exiting wakefulness state after: %r", reason)
--- src/eugeroic/drivers/macos/iokit.py
+++ src/eugeroic/drivers/macos/iokit.py
@@ -28,6 +28,13 @@ def load_iokit():
     iokit.IOPMAssertionRelease.argtypes = [
         ctypes.c_uint32  # IOPMAssertionID
     ]
+
+    iokit.IOPMAssertionDeclareUserActivity.argtypes = [
+        ctypes.c_void_p,                 # CFStringRef
+        ctypes.c_uint32,                 # IOPMUserActiveType
+        ctypes.POINTER(ctypes.c_uint32)  # IOPMAssertionID
+    ]
+
     return iokit
 
 
@@ -44,6 +51,12 @@ class AssertionType(StrEnum):
     PreventSystemSleep = "PreventSystemSleep"
     NoIdleSleep = "NoIdleSleepAssertion"
     NoDisplaySleep = "NoDisplaySleepAssertion"
+
+
+class UserActiveType(IntEnum):
+    """IOPMUserActiveType"""
+    Local = 0
+    Remote = 1
 
 
 def assertion_create_name(iokit, assertion_type: str, level: Level, reason: str):
@@ -65,6 +78,18 @@ def assertion_release(iokit, assertion_id: int):
         raise RuntimeError(f"Failed to release assertion: {assertion_id}")
 
 
+def assertion_declare_user_activity(iokit, assertion_name: str, user_active_type: UserActiveType):
+    assertion_id = ctypes.c_uint32()
+    status = iokit.IOPMAssertionDeclareUserActivity(
+        cfstringref(assertion_name),
+        user_active_type,
+        ctypes.byref(assertion_id)
+    )
+    if status != 0:
+        raise RuntimeError(f"Failed to declare user activity: {assertion_name!r}")
+    return assertion_id
+
+
 @contextlib.contextmanager
 def power_management_assertion(assertion_type: AssertionType, level, reason):
     iokit = get_iokit()
@@ -75,7 +100,15 @@ def power_management_assertion(assertion_type: AssertionType, level, reason):
         assertion_release(iokit, assertion_id)
 
 
+def declare_local_user_activity(reason):
+    """Declare local activity to wake the screen."""
+    iokit = get_iokit()
+    assertion_declare_user_activity(iokit, reason, UserActiveType.Local)
+    print("Declared local user activity.")
+
+
 def main():
+    declare_local_user_activity("Wake up!")
     with power_management_assertion(AssertionType.NoDisplaySleep, Level.On, "Busy busy busy..."):
         for i in range(600):
             print(i)
